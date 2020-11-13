9bfff6678f54c5a16e031f0f892015c01b462442
LanaGomes <lana.gomes1919@gmail.com>
2020-11-12 22:42:44 -0300

    First chore done

---
+++ basics-assignment-1-start/basics-assignment-1-start/assignment.js
@@ -0,0 +1,9 @@
+let fictionalNumber = 8;
+let myResult ; 
+
+myResult = ((18 + fictionalNumber) - 1 * 2) / 4;
+
+alert (`My result is ${myResult} !`)
+alert (`The number you input is ${fictionalNumber}`)
+
+
