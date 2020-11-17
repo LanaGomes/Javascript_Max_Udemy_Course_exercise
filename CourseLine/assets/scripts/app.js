cd63724474a9cbfd3e06bb47478041f06b7449a9
LanaGomes <lana.gomes1919@gmail.com>
2020-11-16 22:20:08 -0300

    My first conscious function!

--- CourseLine/assets/scripts/app.js
+++ CourseLine/assets/scripts/app.js
@@ -2,7 +2,17 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
+function addNumbers(num1, num2){
+    const result = num1 + num2;
+    alert('The result is ' + result);
+}
+
+addNumbers(10,2);
+addNumbers(5,5);
+
 let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult,calculationDescription);
+
+
  
