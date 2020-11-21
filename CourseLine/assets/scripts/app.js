45bf53776ed88b9cc2e96dde363362c4dc6682f3
LanaGomes <lana.gomes1919@gmail.com>
2020-11-21 20:30:51 -0300

    finished calculator and refactored code

--- CourseLine/assets/scripts/app.js
+++ CourseLine/assets/scripts/app.js
@@ -1,20 +1,45 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function addNumbers(){
-    currentResult = currentResult + userInput.value;
-    outputResult (currentResult, '');
- }
-
-addBtn.addEventListener('click', addNumbers);
-
-function theCall (mae, irmao, marido) {
-    let calling = `I love my ${mae}, my brother ${irmao} is my 
-    faithful squire and my husband ${marido} is the best`
-    return calling
+function getUserNumberInput() {
+    return parseInt(userInput.value);
 }
 
-alert(theCall(Leonir,Gabriel,João));
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;/*Toda ${} transforma automaticamente qualquer entrada em string*/
+    outputResult (currentResult, calcDescription); //from vendor file
+}
 
+function addNumbers(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber /*toda variavel "puxada" do HTML  terá o valor de string, é necessário converte-la*/;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+ }
 
+ function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult
+    currentResult = currentResult - enteredNumber /*toda variavel "puxada" do HTML  terá o valor de string, é necessário converte-la*/;
+    createAndWriteOutput ('-', initialResult, enteredNumber);
+ }
  
+ function multiply () {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult
+    currentResult = currentResult * enteredNumber /*toda variavel "puxada" do HTML  terá o valor de string, é necessário converte-la*/;
+    createAndWriteOutput ('*', initialResult, enteredNumber);
+ }
+ function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult
+    currentResult = currentResult / enteredNumber /*toda variavel "puxada" do HTML  terá o valor de string, é necessário converte-la*/;
+    createAndWriteOutput ('/', initialResult, enteredNumber);
+ }
+ 
+addBtn.addEventListener('click', addNumbers);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
+
